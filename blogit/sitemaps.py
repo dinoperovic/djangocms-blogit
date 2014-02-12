529b83b519c66b5bf425e2952286d5b8aa31f144
Dino Perovic <dino.perovic@gmail.com>
2014-02-12 11:15:22 +0100

    Added simple sitemap

---
+++ blogit/sitemaps.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.sitemaps import Sitemap
+
+from blogit.models import Post
+
+
+class BlogitSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'weekly'
+
+    def items(self):
+        return Post.objects.public()
+
+    def lastmod(self, obj):
+        return obj.last_modified

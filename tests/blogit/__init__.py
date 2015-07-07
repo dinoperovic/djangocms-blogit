7e984cdec345d774ffcb91c947f08ad1ae94426e
Dino Perovic <dino.perovic@gmail.com>
2015-07-07 14:37:00 +0200

    add 1 test

--- tests/blogit/__init__.py
+++ tests/blogit/__init__.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from tests.unit.models import (
-    AuthorTestCase, CategoryTestCase, TagTestCase, TaggedPostTestCase,
-    PostTestCase)
-
-
-__all__ = [
-    'AuthorTestCase', 'CategoryTestCase', 'TagTestCase', 'TaggedPostTestCase',
-    'PostTestCase',
-]
+from tests.blogit.models import *  # noqa
--- tests/blogit/models.py
+++ tests/blogit/models.py
@@ -2,4 +2,19 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
-from django.utils.translation import activate
+from django.utils.text import slugify
+
+from blogit.models import Category
+
+
+def create_category(name, parent=None):
+    return Category.objects.create(name=name, slug=slugify(name), parent=None)
+
+
+class TestCategory(TestCase):
+    def setUp(self):
+        self.cat_food = create_category('Food')
+
+    def test_get_absolute_url(self):
+        self.assertEquals(
+            self.cat_food.get_absolute_url(), '/en/categories/food/')

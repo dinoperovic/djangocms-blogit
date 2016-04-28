55bd57aac3689020594c8e4c23642b102f6c77c6
Dino Perovic <dino.perovic@gmail.com>
2016-04-28 10:18:25 +0200

    Bump to 0.4.4

--- blogit/__init__.py
+++ blogit/__init__.py
@@ -4,15 +4,15 @@ from __future__ import unicode_literals
 """
 Release logic:
  1. Remove ".devX" from __version__ (below)
- 2. git add shop/__init__.py
+ 2. git add blogit/__init__.py
  3. git commit -m 'Bump to <version>'
  4. git tag <version>
  5. git push --tags
  6. python setup.py sdist upload
  7. bump the __version__, append ".dev0"
- 8. git add shop/__init__.py
+ 8. git add blogit/__init__.py
  9. git commit -m 'Start with <version>'
 10. git push
 """
 
-__version__ = '0.4.4.dev0'
+__version__ = '0.4.4'
